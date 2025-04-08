10e646ebb3a6a0d0c0231e209fb06bbfcc90b9a3
blaze <blaze121304@gmail.com>
2025-04-08 23:42:30 +0900

    feat: open-ai-gps JPA 관련 클래스 신규 생성

--- openai-api-gps/src/main/java/com/rusty/openaiapigps/domain/entity/GpsData.java
+++ openai-api-gps/src/main/java/com/rusty/openaiapigps/domain/entity/GpsData.java
@@ -1,24 +1,24 @@
-//package com.rusty.openaiapigps.domain.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Data
-//public class GpsData {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private double latitude;
-//    private double longitude;
-//    private LocalDateTime timestamp = LocalDateTime.now();
-//}
+package com.rusty.openaiapigps.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class GpsData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double latitude;
+    private double longitude;
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
